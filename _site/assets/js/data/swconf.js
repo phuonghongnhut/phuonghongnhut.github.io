aba9c9ce2df56a125ce93737e0514081c184537f
HongNhut <hongnhut@gmail.com>
2026-01-02 14:50:54 +0700

    Add post

--- _site/assets/js/data/swconf.js
+++ _site/assets/js/data/swconf.js
@@ -1,6 +1,6 @@
 const swconf = {
   
-    cacheName: 'chirpy-1767339711',resources: [
+    cacheName: 'chirpy-1767340228',resources: [
       '/assets/css/jekyll-theme-chirpy.css',
       '/',
       
